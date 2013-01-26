bb2cdb06091732d5e2817e10a745fcd2c0050629
Strilanc <craig.gidney@gmail.com>
2013-01-26 01:11:51 -0400

    Dead code

--- Quantum/Circuit/ICircuitElement.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface ICircuitElement<T> {
-    Superposition<T> Apply(T state);
-    IReadOnlyList<Wire> Inputs { get; }
-}
--- Quantum/Circuit/Wire.cs
+++
@@ -1,9 +0,0 @@
-﻿public sealed class Wire {
-    public readonly string Name;
-    public Wire(string name = null) {
-        this.Name = name;
-    }
-    public override string ToString() {
-        return this.Name ?? base.ToString();
-    }
-}
