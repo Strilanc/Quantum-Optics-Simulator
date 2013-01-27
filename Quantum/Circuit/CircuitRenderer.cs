8e75fc86dbfa81b4d0a8cb9cf2e28facf94519aa
Strilanc <craig.gidney@gmail.com>
2013-01-27 00:26:18 -0400

    polarization

--- Quantum/Circuit/CircuitRenderer.cs
+++ Quantum/Circuit/CircuitRenderer.cs
@@ -57,6 +57,7 @@ namespace Quantum {
                     _cells[i, j] = new Cell { X = i, Y = j, State = CellState.Empty };
                 }
             }
+            ComputeCircuit();
         }
 
         public bool EnableClear { get; set; }
@@ -69,27 +70,6 @@ namespace Quantum {
             this._clock = Stopwatch.StartNew();
         }
 
-        private void InitPathGeometry(RenderParams renderParams, float sizeX) {
-            var sizeShape = sizeX / 4;
-
-            // Creates a random geometry inside a circle
-            _pathGeometry1 = new PathGeometry1(renderParams.DirectXResources.FactoryDirect2D);
-
-            var pathSink = _pathGeometry1.Open();
-            var startingPoint = new DrawingPointF(sizeShape / 2, 0);
-            pathSink.BeginFigure(startingPoint, FigureBegin.Hollow);
-            foreach (var i in 128.Range()) {
-                var angle = i * Tau / 128;
-                var b = (i & 1) != 0;
-                var r = sizeShape * (float)(b ? Math.Sin(angle * 6) * 0.1 + 0.9 : Math.Cos(angle) * 0.1 + 0.4);
-                var theta = angle + (b ? Tau / 24 : 0);
-                pathSink.AddLine(new DrawingPointF(
-                    r * (float)Math.Cos(theta),
-                    r * (float)Math.Sin(theta)));
-            }
-            pathSink.EndFigure(FigureEnd.Open);
-            pathSink.Close();
-        }
         public virtual void Render(RenderParams renderParams) {
             var t = (float)_clock.Elapsed.TotalSeconds;
             if (!Show) return;
@@ -100,41 +80,37 @@ namespace Quantum {
             if (EnableClear) context2D.Clear(Color.Black);
 
             var r = renderParams.SizedDeviceResources.RenderTargetBounds;
-            var sizeX = (float)r.Width;
-            var sizeY = (float)r.Height;
-            var centerX = (float)(r.X + sizeX/2);
-            var centerY = (float)(r.Y + sizeY/2);
-
-            _textFormat = _textFormat ?? new TextFormat(renderParams.DirectXResources.FactoryDirectWrite, "Calibri", 16*sizeX/1920) {
+            _textFormat = _textFormat ?? new TextFormat(renderParams.DirectXResources.FactoryDirectWrite, "Calibri", 16*(float)r.Width/1920) {
                 TextAlignment = TextAlignment.Center,
                 ParagraphAlignment = ParagraphAlignment.Center
             };
-            if (_pathGeometry1 == null) InitPathGeometry(renderParams, sizeX);
-
             context2D.TextAntialiasMode = TextAntialiasMode.Grayscale;
-            //context2D.Transform = Matrix.RotationZ((float)(Math.Cos(t * Tau / 2))) * Matrix.Translation(centerX, centerY, 0);
-            context2D.DrawText(Message, _textFormat, new RectangleF(-sizeX/2, -sizeY/2, +sizeX/2, sizeY/2), _sceneColorBrush);
+            context2D.DrawText(Message, _textFormat, new RectangleF((float)r.Left, (float)r.Top, (float)r.Right, (float)r.Bottom), _sceneColorBrush);
 
             using (PathGeometry1 sineWave = new PathGeometry1(renderParams.DirectXResources.FactoryDirect2D),
                                  cosineWave = new PathGeometry1(renderParams.DirectXResources.FactoryDirect2D)) {
-                const int ni = 100;
+                const int Precision = 100;
+
+                // sine wave shape
                 var pathSink = sineWave.Open();
-                pathSink.BeginFigure(new DrawingPointF(0, 0.5f*100), FigureBegin.Filled);
-                foreach (var i in ni.Range()) {
-                    var x = ((float)i+1)/ni;
-                    var y = (float)Math.Sin(x * Tau) * 0.5f + 0.5f;
-                    pathSink.AddLine(new DrawingPointF(100 * x, 100 * y));
+                pathSink.BeginFigure(new DrawingPointF(0, Precision / 2.0f), FigureBegin.Filled);
+                foreach (var i in Precision.Range()) {
+                    var x = i + 1.0f;
+                    var y = Precision * (float)(Math.Sin(x / Precision * Tau) + 1) / 2;
+                    pathSink.AddLine(new DrawingPointF(x, y));
                 }
                 pathSink.EndFigure(FigureEnd.Open);
                 pathSink.Close();
 
+                // cosine wave shape
                 pathSink = cosineWave.Open();
-                pathSink.BeginFigure(new DrawingPointF(0, 0), FigureBegin.Filled);
-                foreach (var i in ni.Range()) {
-                    var x = ((float)i+1)/ni;
-                    var y = (float)Math.Cos(x * Tau) * 0.5f + 0.5f;
-                    pathSink.AddLine(new DrawingPointF(100 * x, 100 * y));
+                pathSink.BeginFigure(new DrawingPointF(0, Precision / 2.0f), FigureBegin.Filled);
+                foreach (var i in Precision.Range()) {
+                    var x = i + 1.0f;
+                    var y = Precision * (float)(Math.Cos(x / Precision * Tau) + 1) / 2;
+                    pathSink.AddLine(new DrawingPointF(x, y));
                 }
+                pathSink.AddLine(new DrawingPointF(Precision, Precision / 2.0f));
                 pathSink.EndFigure(FigureEnd.Open);
                 pathSink.Close();
 
@@ -145,31 +121,28 @@ namespace Quantum {
                                        brush3 = new SolidColorBrush(renderParams.DevicesAndContexts.ContextDirect2D, new Color(255, 0, 0, 64))) {
 
                     foreach (var p in Waves) {
-                        var a = (float)Math.Min(1, Math.Max(0, p.Item2.Magnitude));
-                        var c = p.Item1.Pos;
-                        var v = p.Item1.Vel;
-                        var x1 = w*(c.X - v.X + 0.5f);
-                        var y1 = h*(c.Y - v.Y + 0.5f);
-                        var x2 = w*(c.X + 0.5f);
-                        var y2 = h*(c.Y + 0.5f);
+                        var amps = (float)Math.Min(1, Math.Max(0, p.Item2.Magnitude));
+                        var rot = (float)Dir.FromVector(-p.Item1.Vel.X, -p.Item1.Vel.Y).SignedNaturalAngle;
+                        var x = w*(p.Item1.Pos.X + 0.5f);
+                        var y = h*(p.Item1.Pos.Y + 0.5f);
 
                         context2D.Transform =
-                              Matrix.Scaling(1 / 100.0f)
+                              Matrix.Scaling(1.0f / Precision)
                             * Matrix.Translation(0, -0.5f, 0)
-                            * Matrix.Scaling(1, a * a * (float)p.Item1.Pol.Dir.UnitX, 1)
-                            * Matrix.RotationZ((float)Dir.FromVector(v.X, v.Y).SignedNaturalAngle)
+                            * Matrix.Scaling(1, amps * amps * (float)p.Item1.Pol.Dir.UnitX, 1)
+                            * Matrix.RotationZ(rot)
                             * Matrix.Scaling(w, h, 1)
-                            * Matrix.Translation(x1, y1, 0);
+                            * Matrix.Translation(x, y, 0);
                         context2D.FillGeometry(sineWave, brush2);
                         context2D.DrawGeometry(sineWave, brush);
 
                         context2D.Transform =
-                              Matrix.Scaling(1 / 100.0f)
+                              Matrix.Scaling(1.0f / Precision)
                             * Matrix.Translation(0, -0.5f, 0)
-                            * Matrix.Scaling(1, a * a * (float)p.Item1.Pol.Dir.UnitY, 1)
-                            * Matrix.RotationZ((float)Dir.FromVector(v.X, v.Y).SignedNaturalAngle)
+                            * Matrix.Scaling(1, amps * amps * (float)p.Item1.Pol.Dir.UnitY, 1)
+                            * Matrix.RotationZ(rot)
                             * Matrix.Scaling(w, h, 1)
-                            * Matrix.Translation(x1, y1, 0);
+                            * Matrix.Translation(x, y, 0);
                         context2D.FillGeometry(cosineWave, brush3);
                         context2D.DrawGeometry(cosineWave, brush);
                     }
@@ -238,7 +211,7 @@ namespace Quantum {
                     return x2;
                 });
 
-            var initialState = new CircuitState(TimeSpan.Zero, new Photon(new Position(0, 0), Velocity.PlusX, default(Polarization)));
+            var initialState = new CircuitState(TimeSpan.Zero, new Photon(new Position(0, 10), Velocity.PlusX, default(Polarization)));
             foreach (var e in AllCells)
                 e.Trace = 0;
             Waves.Clear();
